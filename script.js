e68b04b7f517845d82d90fdaeca98da6f850ca6a
Memduh Emir Yavuz <167324640+memduhemir@users.noreply.github.com>
2024-12-15 00:16:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "Correct Choice!";
+    gif.src = "trumpGif.gif";
+    yesBtn.style.display = "none";
+    noBtn.style.display = "none";
+});
+
+
+
+noBtn.addEventListener("mouseover", () => {
+    const wrapper = document.querySelector(".wrapper");
+    const wrapperRect = wrapper.getBoundingClientRect();
+    const noBtnRect = noBtn.getBoundingClientRect();
+  
+   
+    const maxX = wrapperRect.width - noBtnRect.width;
+    const maxY = wrapperRect.height - noBtnRect.height;
+  
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+  
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+  });
+
+
+
 
 		var xOff = 5;
 		var yOff = 5;
@@ -11,12 +44,12 @@ function openWindow(url){
 }
 
 function procreate(){
-		openWindow('https://adafirdevs1.github.io/ChooseNo/');
-		openWindow('https://adafirdevs1.github.io/ChooseNo/');
-		openWindow('https://adafirdevs1.github.io/ChooseNo/');
-		openWindow('https://adafirdevs1.github.io/ChooseNo/');
-		openWindow('https://adafirdevs1.github.io/ChooseNo/');
-		openWindow('https://adafirdevs1.github.io/ChooseNo/');
+		openWindow('open.html');
+		openWindow('open.html');
+		openWindow('open.html');
+		openWindow('open.html');
+		openWindow('open.html');
+		openWindow('open.html');
 		}
 
 function newXlt(){
